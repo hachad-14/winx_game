041bf54f80d0d29278fef852a3e478374ec17441
hachad-14 <hachadsaad14@gmail.com>
2022-04-21 03:24:51 +0000

    sf

--- lib/api/acQuestions.dart
+++ lib/api/acQuestions.dart
@@ -1,8 +1,14 @@
 import "dart:math";
 
 var questionsList = [
-  'Mange ma bite',
-  'Spaka le plus beau',
-  'Fais un bisou a Spaka',
-  'Spaka aime Ayou',
+  'Laisse le joueur de ton choix te faire des chatouilles pendant une minute !',
+  'Fais un concours de celui qui dira le plus de gros mots contre le joueur de ton choix !',
+  'Prends le téléphone du joueur de ton choix et fais-y ce que tu veux pendant 30 secondes !',
+  'Choisis un vêtement et enlève-le !',
+  'Laisse le joueur de ton choix te dessiner quelque chose sur le visage !',
+  'Fais une fausse déclaration d’amour au joueur de ton choix  !',
+  'Retire un sous-vêtement sans enlever tes vêtements !',
+  'Ferme les yeux. Mets-toi au milieu des joueurs, tourne sur toi même, et fais un câlin à la personne en face de toi quand tu t’arrêtes. !',
+  'Fais un compliment à chacun des joueurs !',
+  'Reste assis(e) sur les genoux d’un des joueurs de ton choix pendant un tour !',
 ];
